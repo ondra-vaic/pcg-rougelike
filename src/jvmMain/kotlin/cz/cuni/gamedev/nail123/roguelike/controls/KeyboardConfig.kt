f232e11e76d80956ae8501a023f8772d0207be06
Woitee <woitee@gmail.com>
2020-04-23 15:20:09 +0200

    Refactored default controls to KeyboardConfig

--- src/jvmMain/kotlin/cz/cuni/gamedev/nail123/roguelike/controls/KeyboardConfig.kt
+++ src/jvmMain/kotlin/cz/cuni/gamedev/nail123/roguelike/controls/KeyboardConfig.kt
@@ -1,4 +1,28 @@
 package cz.cuni.gamedev.nail123.roguelike.controls
 
-class KeyboardConfig {
+import cz.cuni.gamedev.nail123.roguelike.Game
+import org.hexworks.zircon.api.uievent.KeyCode
+
+class KeyboardConfig(val mapping: Map<KeyCode, Game.PlayerAction>) {
+    companion object {
+        val Default = KeyboardConfig(mapOf(
+            // Default 8-directional movement (WASD + QEZC)
+            KeyCode.KEY_W to Game.PlayerAction.MOVE_NORTH,
+            KeyCode.KEY_A to Game.PlayerAction.MOVE_WEST,
+            KeyCode.KEY_S to Game.PlayerAction.MOVE_SOUTH,
+            KeyCode.KEY_D to Game.PlayerAction.MOVE_EAST,
+            KeyCode.KEY_E to Game.PlayerAction.MOVE_NORTHEAST,
+            KeyCode.KEY_C to Game.PlayerAction.MOVE_SOUTHEAST,
+            KeyCode.KEY_Z to Game.PlayerAction.MOVE_SOUTHWEST,
+            // Supporting czech keyboard layout
+            KeyCode.KEY_Y to Game.PlayerAction.MOVE_SOUTHWEST,
+            KeyCode.KEY_Q to Game.PlayerAction.MOVE_NORTHWEST,
+
+            // 4-directional movement also on arrow keys
+            KeyCode.UP to Game.PlayerAction.MOVE_NORTH,
+            KeyCode.LEFT to Game.PlayerAction.MOVE_WEST,
+            KeyCode.DOWN to Game.PlayerAction.MOVE_SOUTH,
+            KeyCode.RIGHT to Game.PlayerAction.MOVE_EAST
+        ))
+    }
 }
--- src/jvmMain/kotlin/cz/cuni/gamedev/nail123/roguelike/controls/KeyboardControls.kt
+++ src/jvmMain/kotlin/cz/cuni/gamedev/nail123/roguelike/controls/KeyboardControls.kt
@@ -7,28 +7,12 @@ import org.hexworks.zircon.api.uievent.UIEvent
 import org.hexworks.zircon.api.uievent.UIEventResponse
 
 class KeyboardControls(val game: Game): Controls() {
-    // TODO: this should be what KeyboardConfig is for
-    val mapping = mapOf(
-            KeyCode.KEY_W to Game.PlayerAction.MOVE_NORTH,
-            KeyCode.KEY_A to Game.PlayerAction.MOVE_WEST,
-            KeyCode.KEY_S to Game.PlayerAction.MOVE_SOUTH,
-            KeyCode.KEY_D to Game.PlayerAction.MOVE_EAST,
-            KeyCode.KEY_E to Game.PlayerAction.MOVE_NORTHEAST,
-            KeyCode.KEY_C to Game.PlayerAction.MOVE_SOUTHEAST,
-            KeyCode.KEY_Z to Game.PlayerAction.MOVE_SOUTHWEST,
-            // Supporting czech keyboard layout
-            KeyCode.KEY_Y to Game.PlayerAction.MOVE_SOUTHWEST,
-            KeyCode.KEY_Q to Game.PlayerAction.MOVE_NORTHWEST,
-            KeyCode.UP to Game.PlayerAction.MOVE_NORTH,
-            KeyCode.LEFT to Game.PlayerAction.MOVE_WEST,
-            KeyCode.DOWN to Game.PlayerAction.MOVE_SOUTH,
-            KeyCode.RIGHT to Game.PlayerAction.MOVE_EAST
-    )
+    var config = KeyboardConfig.Default
 
     override fun handleInput(input: UIEvent): UIEventResponse {
         // Validate whether the key pressed is valid
         if (input !is KeyboardEvent) return UIEventResponse.pass()
-        val action = mapping[input.code] ?: return UIEventResponse.pass()
+        val action = config.mapping[input.code] ?: return UIEventResponse.pass()
         // Perform the action
         game.step(action)
         return UIEventResponse.processed()
