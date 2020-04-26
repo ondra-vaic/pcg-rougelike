09269343b1a6b127eb0a4f6ea940f46444d8b3ed
Woitee <woitee@gmail.com>
2020-04-26 17:52:13 +0200

    Autotiling considers the outside as belonging to currently tiled

--- src/jvmMain/kotlin/cz/cuni/gamedev/nail123/roguelike/tiles/Autotiling.kt
+++ src/jvmMain/kotlin/cz/cuni/gamedev/nail123/roguelike/tiles/Autotiling.kt
@@ -1,5 +1,6 @@
 package cz.cuni.gamedev.nail123.roguelike.tiles
 
+import cz.cuni.gamedev.nail123.roguelike.blocks.GameBlock
 import cz.cuni.gamedev.nail123.roguelike.blocks.Wall
 import cz.cuni.gamedev.nail123.roguelike.extensions.shift
 import cz.cuni.gamedev.nail123.roguelike.world.Direction
@@ -7,10 +8,11 @@ import org.hexworks.zircon.api.data.Tile
 import kotlin.reflect.KProperty
 
 class Autotiling(val default: Tile, vararg val mapping: Pair<Int, Tile>) {
-    operator fun getValue(wall: Wall, property: KProperty<*>): Tile {
+    operator fun getValue(block: GameBlock, property: KProperty<*>): Tile {
         var flags = 0
         for (direction in Direction.eightDirections) {
-            if (wall.area[wall.position shift direction]?.javaClass == wall.javaClass) {
+            val targetClass = block.area[block.position shift direction]?.javaClass
+            if (targetClass == null || targetClass == block.javaClass) {
                 flags = flags or direction.flag
             }
         }
@@ -24,7 +26,7 @@ class Autotiling(val default: Tile, vararg val mapping: Pair<Int, Tile>) {
         return default
     }
 
-    operator fun setValue(wall: Wall, property: KProperty<*>, tile: Tile) {
+    operator fun setValue(wall: GameBlock, property: KProperty<*>, tile: Tile) {
         // Do nothing
     }
 }
