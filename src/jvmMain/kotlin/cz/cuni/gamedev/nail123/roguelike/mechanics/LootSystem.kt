73cc8490c2db45929ed068b4babe9207a8155d0c
Ondrej <ondra.vaic1@gmail.com>
2022-05-24 16:03:20 +0200

    drops set

--- src/jvmMain/kotlin/cz/cuni/gamedev/nail123/roguelike/mechanics/LootSystem.kt
+++ src/jvmMain/kotlin/cz/cuni/gamedev/nail123/roguelike/mechanics/LootSystem.kt
@@ -179,46 +179,82 @@ object LootSystem {
     val enemyDrops = mapOf(
         Rat::class to mapOf(
             1 to TreasureClass(1, listOf(
-                2 to NoDrop,
-                1 to veryRareShield(1))
+                4 to NoDrop,
+                2 to ring(1),
+                1 to rareRing(1),
+                4 to veryRareRing(1))
             ),
             2 to TreasureClass(1, listOf(
-                2 to NoDrop,
-                1 to sword(2))
+                5 to NoDrop,
+                5 to ring(2),
+                3 to rareRing(2),
+                1 to veryRareRing(2))
             ),
             3 to TreasureClass(1, listOf(
-                2 to NoDrop,
-                1 to sword(3))
+                4 to NoDrop,
+                5 to ring(3),
+                3 to rareRing(3),
+                1 to veryRareRing(3))
             ),
             4 to TreasureClass(1, listOf(
-                2 to NoDrop,
-                1 to sword(4))
+                3 to NoDrop,
+                4 to ring(4),
+                2 to rareRing(4),
+                1 to veryRareRing(4))
             ),
             5 to TreasureClass(1, listOf(
                 2 to NoDrop,
-                1 to sword(5))
+                2 to ring(5),
+                3 to rareRing(5),
+                2 to veryRareRing(5))
             )
         ),
+
         Orc::class to mapOf(
             1 to TreasureClass(1, listOf(
-                2 to NoDrop,
-                1 to sword(1))
+                8 to NoDrop,
+                5 to sword(1),
+                2 to rareSword(1),
+                1 to veryRareSword(1))
             ),
             2 to TreasureClass(1, listOf(
-                2 to NoDrop,
-                1 to sword(2))
+                13 to NoDrop,
+                4 to body(2),
+                2 to rareBody(2),
+                4 to sword(2),
+                2 to veryRareSword(2))
             ),
             3 to TreasureClass(1, listOf(
-                2 to NoDrop,
-                1 to sword(3))
+                16 to NoDrop,
+                4 to shield(3),
+                2 to rareShield(3),
+                4 to sword(3),
+                2 to rareSword(3),
+                1 to veryRareSword(3))
             ),
             4 to TreasureClass(1, listOf(
-                2 to NoDrop,
-                1 to sword(4))
+                20 to NoDrop,
+                4 to shield(4),
+                2 to rareShield(4),
+                1 to veryRareShield(4),
+                4 to body(4),
+                2 to rareBody(4),
+                1 to veryRareBody(4),
+                4 to sword(4),
+                2 to rareSword(4),
+                1 to veryRareSword(4))
             ),
             5 to TreasureClass(1, listOf(
-                2 to NoDrop,
-                1 to sword(5))
+                26 to NoDrop,
+                5 to shield(5),
+                3 to rareShield(5),
+                2 to veryRareShield(5),
+                5 to body(5),
+                3 to rareBody(5),
+                2 to veryRareBody(5),
+                5 to sword(5),
+                3 to rareSword(5),
+                2 to veryRareSword(5))
             )
         ),
     )
